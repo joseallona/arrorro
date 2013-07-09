436ffb25fe072b07d6312974da4bb63212d489fd
joseallona <jallona@gmail.com>
2013-07-09 20:45:04 -0300

    up3

--- func.php
+++ func.php
@@ -2,7 +2,7 @@
 
 $ADMIN_PASS = "miniño2009";
 
-@mysql_connect("0.0.0.0:3306","root","root") or die("Error MySQL Connect");
+@mysql_connect("0.0.0.0:3306","db_arrorro","arrorro123456") or die("Error MySQL Connect");
 @mysql_select_db("site") or die("Error MySQL Select DB");
 
 ?>
