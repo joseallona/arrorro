66229a28900223f2505d01209e8ba6ceebae0566
joseallona <jallona@gmail.com>
2013-07-09 21:45:05 -0300

    cambio p de adminstrador

--- func.php
+++ func.php
@@ -1,6 +1,6 @@
 <?
 
-$ADMIN_PASS = "miniño2009";
+$ADMIN_PASS = "admin123";
 
 @mysql_connect("201.235.253.27:3306","db_arrorro","arrorro123456") or die("Error MySQL Connect");
 @mysql_select_db("site") or die("Error MySQL Select DB");
