b6948d91b93825c6cecc150ce1649547037cc1c1
joseallona <jallona@gmail.com>
2013-10-02 17:56:14 -0300

    mando shellexec por mail

--- upload.php
+++ upload.php
@@ -30,8 +30,8 @@ if (!empty($_FILES)) {
 		$second = rand(1, ($total - 1));
 	}
 	$cmd = "$ffmpeg -i $video -deinterlace -an -ss $second -t 00:00:01 -r 1 -y -s 320x240 -vcodec mjpeg -f mjpeg $image 2>&1";
-	mail('jallona@gmail.com','test', $cmd);
-	shell_exec($cmd);
+	mail('jallona@gmail.com','test', shell_exec($cmd));
+	//shell_exec($cmd);
 	
 	//echo $video;
 
